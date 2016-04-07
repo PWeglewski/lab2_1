1a7574773e9a6a3150d3429787c74c3fce9987c3
Piotr Weglewski <190160@edu.p.lodz.pl>
2016-04-07 18:15:59 +0200

    Added test case for non empty sequence and existing value, fixed generateSequence method to eliminate duplicates in generated sequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Random;
 
+import static java.lang.Math.abs;
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by piotr on 07.04.2016.
  */
@@ -17,13 +20,45 @@ public class BinarySearchTest {
         BinarySearch.search(key, seq);
     }
 
+    @Test
+    public void shouldFindExistingKey() throws Exception{
+        int size = 10;
+
+        int[] seq = generateSequence(size);
+
+        Random random = new Random();
+
+        int index = abs(random.nextInt())%size;
+
+        int value = seq[index];
+
+        SearchResult searchResult = BinarySearch.search(value, seq);
+
+        assertEquals(true, searchResult.isFound());
+        assertEquals(index, searchResult.getPosition());
+    }
+
     public int[] generateSequence(int size) {
         int[] sequence = new int[size];
 
         Random random = new Random();
+        boolean duplicateFlag;
 
-        for (int element : sequence) {
-            element = random.nextInt();
+        for (int i = 0; i < size; i++) {
+            int randomValue;
+
+            do {
+                duplicateFlag = false;
+                randomValue = random.nextInt();
+                for (int j = 0; j < i; j++) {
+                    if (sequence[j] == randomValue) {
+                        duplicateFlag = true;
+                        break;
+                    }
+                }
+            } while (duplicateFlag);
+
+            sequence[i] = randomValue;
         }
 
         Arrays.sort(sequence);
